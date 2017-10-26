0e7a5a8c6dece7dc5575b325da891e62e7d79e9d
damys <damys@foxmail.com>
2017-10-26 16:33:22 +0800

    update

--- lab/pages/lab/index.js
+++ lab/pages/lab/index.js
@@ -6,5 +6,10 @@ Page({
     // 模块引入测试
     test.sayHello('jack');
     test.sayGoodby('jack');
-  }
+  },
+
+  handleTap1: function (event) { console.log('handleTap1') },
+  handleTap2: function (event) { console.log('handleTap2') },
+  handleTap3: function (event) { console.log('handleTap3') },
+
 })
