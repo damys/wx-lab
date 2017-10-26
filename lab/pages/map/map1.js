ac6dd70bdaf91b98f5b08e8d85967f779db0bdb1
damys <damys@foxmail.com>
2017-10-26 16:32:33 +0800

    update

---
+++ lab/pages/map/map1.js
@@ -0,0 +1,117 @@
+Page({
+  data: {
+    markers: [{
+      iconPath: "/style/temp/location.png",
+      id: 0,
+      latitude: 22.541040,
+      longitude: 114.091070,
+      width: 50,
+      height: 50
+    }],
+
+
+    polyline: [{
+      points: [{
+        latitude: 22.541040,
+        longitude: 114.091070
+      }, {
+        latitude: 23.10229,
+        longitude: 113.3345211
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: "/style/temp/location.png",
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
+  },
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  },
+
+  onReady: function (e) {
+    // 使用 wx.createMapContext 获取 map 上下文
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function (res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+
+    wx.getLocation({
+      type: 'gcj02',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+
+        console.log(res)
+      }
+    })
+  },
+
+  moveToLocation1: function () {
+    wx.chooseLocation({
+      type: 'gcj02',      //返回可以用于wx.openLocation的经纬度
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      }
+    })
+  },
+
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
+  },
+
+  translateMarker: function () {
+    this.mapCtx.translateMarker({
+      markerId: 0,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function () {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      }, {
+        latitude: 23.00229,
+        longitude: 113.3345211,
+      }]
+    })
+  }
+})
